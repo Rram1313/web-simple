26277ddb8b0e63a7a415d522fb29da2a2d1e86d8
Rram1313 <ramaruthdn1a@gmail.com>
2024-04-12 09:46:51 +0200

    subir

---
+++ src/pages/servicios.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ServiciosPage = () => (
+  <Layout>
+    <h1>Nuestros Servicios</h1>
+    <p>En Desarrollo Web, ofrecemos una amplia gama de servicios para ayudarte a alcanzar tus objetivos en línea. Algunos de nuestros servicios incluyen:</p>
+    <ul>
+      <li>Diseño y desarrollo de sitios web personalizados</li>
+      <li>Optimización para motores de búsqueda (SEO)</li>
+      <li>Desarrollo de aplicaciones web</li>
+      <li>Integración de sistemas de gestión de contenido</li>
+      <li>Desarrollo de tiendas en línea (e-commerce)</li>
+      <li>Consultoría y asesoramiento en estrategias digitales</li>
+    </ul>
+    <p>Estamos comprometidos a brindar soluciones efectivas y de alta calidad que satisfagan las necesidades únicas de cada cliente. ¡Contáctanos hoy para comenzar!</p>
+  </Layout>
+)
+
+export default ServiciosPage
